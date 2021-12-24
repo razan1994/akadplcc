a37345892b0b3690d82dadcb06ac88d0dfddb53a
unknown <mohd_salah94@outlook.com>
2021-12-24 16:19:32 +0200

    Mohammed Salah 24-12-2021 04:19 AM

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,51 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $table = 'doctors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'username',
+        'email',
+        'phone',
+        'password',
+        'profile_photo_path',
+        'user_status',
+        'created_by',
+        'country_id',
+        'region_id',
+        'address_ar',
+        'address_en',
+        'player_id',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+        'pivot',
+        'profile_photo_url'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 }
--- app/Models/Lab.php
+++ app/Models/Lab.php
@@ -8,4 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Lab extends Model
 {
     use HasFactory;
+    protected $table = 'labs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'username',
+        'email',
+        'phone',
+        'password',
+        'profile_photo_path',
+        'user_status',
+        'created_by',
+        'country_id',
+        'region_id',
+        'address_ar',
+        'address_en',
+        'player_id',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+        'pivot',
+        'profile_photo_url'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 }
