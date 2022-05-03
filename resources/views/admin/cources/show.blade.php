0cffbd7e8a0a8cd247fb1f713d5296d6b9202e83
unknown <mohd_salah94@outlook.com>
2022-05-03 23:04:29 +0300

    Mohammed Salah 03-05-2022 11:04 PM

--- resources/views/admin/cources/show.blade.php
+++ resources/views/admin/cources/show.blade.php
@@ -343,7 +343,7 @@
             query:{_token:'{{ csrf_token() }}'} ,// CSRF token
             maxFilesize: 5*1024*1024*1024,
             fileType: ['mp4'],
-            chunkSize: 10*1024*1024, // default is 1*1024*1024, this should be less than your maximum limit in php.ini
+            chunkSize: 1024*1024, // default is 1*1024*1024, this should be less than your maximum limit in php.ini
             headers: {
                 'Accept' : 'application/json'
             },
