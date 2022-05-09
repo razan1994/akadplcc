26f0bf5b09ede01927ba26551c910916b3fa9444
unknown <mohd_salah94@outlook.com>
2022-05-10 02:10:00 +0300

    Mohammed Salah 10-05-2022 02:09 AM

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,8 +1,9 @@
-<x-guest-layout>
+@extends('front_end_layout.app_front_end', ['title' => 'الصفحة الرئيسية'])
+
+@section('content')
     <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
-                <x-jet-authentication-card-logo />
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
@@ -107,4 +108,4 @@
             </div>
         </div>
     </div>
-</x-guest-layout>
+@endsection
