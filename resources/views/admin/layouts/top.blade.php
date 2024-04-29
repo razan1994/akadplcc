7565c4bab5fbfcca355850e55880105a4493f737
Ahmad Alsakhen <ahmadalsakhen36@gmail.com>
2024-04-29 11:59:45 +0300

    Ahmad Alsakhen 29-4-2024 11:59AM

--- resources/views/admin/layouts/top.blade.php
+++ resources/views/admin/layouts/top.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="description"
         content="Sleek Dashboard - Free Bootstrap 4 Admin Dashboard Template and UI Kit. It is very powerful bootstrap admin dashboard, which allows you to build products like admin panels, content management systems and CRMs etc.">
 
