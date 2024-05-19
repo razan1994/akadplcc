fc8b228cfe0f1e68b7fb80a155577419e2edac55
Ahmad Alsakhen <ahmadalsakhen36@gmail.com>
2024-05-19 14:16:59 +0300

    Ahmad Alsakhen 19-5-2024 2:16PM

--- resources/views/front_end_layout/app_front_end.blade.php
+++ resources/views/front_end_layout/app_front_end.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Kanaf</title>
+    <title>كنف المعرفة - kanaff</title>
     <link rel="shortcut icon" href="{{ asset('front_end_style/images/faviconlogo.png') }}" type="image/png">
 
 
